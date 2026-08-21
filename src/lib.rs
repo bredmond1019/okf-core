be5a8de1b4a2dabc184eb3564f76f8ec311b71bd
Brandon Redmond <bredmond1019@gmail.com>
2026-08-21 07:08:56 -0300

    feat: implement OK.4.A-task4

--- src/lib.rs
+++ src/lib.rs
@@ -24,10 +24,10 @@ pub use graph::{Edge, EdgeKind, EdgeResolution, Graph, GraphArtifact, Node, reso
 pub use graph_emit::{ExportedEdge, GraphExport, build_graph_export};
 pub use parse::{Frontmatter, ParseResult, extract_frontmatter, parse_frontmatter};
 pub use state::{
-    ApprovalDep, Backlog, BacklogOrigin, Block, BlockDep, BlockedBy, Carryover, CarryoverKind,
-    CarryoverScope, ClearsWhen, ClearsWhenPredicate, CrossRepoEdge, Endpoint, Epic, ExternalDep,
-    Focus, KnownCarryoverKind, OperatorDep, Origin, Reference, RepoRollup, StateEdge,
-    StateEdgeKind, StateFile, StateGraph, StateLoadError, StateNode, StateSource, TierEntry, Track,
-    TrackBlock, W_STATE_OP_SLUG_STUTTER, build_state_graph, load_state, normalize_op_slug, op_id,
-    op_slug_stutters,
+    AmendsRef, ApprovalDep, Backlog, BacklogOrigin, Block, BlockDep, BlockedBy, Carryover,
+    CarryoverArchiveRow, CarryoverKind, CarryoverScope, ClearsWhen, ClearsWhenPredicate,
+    CrossRepoEdge, DisposalReason, Endpoint, Epic, ExternalDep, Focus, KnownCarryoverKind,
+    OperatorDep, Origin, Reference, RepoRollup, StateEdge, StateEdgeKind, StateFile, StateGraph,
+    StateLoadError, StateNode, StateSource, TierEntry, Track, TrackBlock, W_STATE_OP_SLUG_STUTTER,
+    build_state_graph, load_state, normalize_op_slug, op_id, op_slug_stutters,
 };
